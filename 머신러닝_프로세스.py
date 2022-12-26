132475bd2f82463e5fe96fab57d9b294c5b5524c
최현우 <107903417+chw8207@users.noreply.github.com>
2022-12-26 15:35:52 +0900

    Colaboratory를 통해 생성됨

---
+++ 머신러닝_프로세스.py
@@ -0,0 +1,44 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "mount_file_id": "1ggYzL9c_a1q63WT1QA_TCQIWswLubCzn",
+      "authorship_tag": "ABX9TyOFbE+GoBeAGevrxpTMgRcz",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    },
+    "accelerator": "GPU",
+    "gpuClass": "standard"
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/chw8207/ADP-study/blob/master/%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D_%ED%94%84%EB%A1%9C%EC%84%B8%EC%8A%A4.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "KxCjPXK-vAOH"
+      },
+      "outputs": [],
+      "source": [
+        "#"
+      ]
+    }
+  ]
+}
